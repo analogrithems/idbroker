0274d1bc4a1407d79610b40152ed5fd976fde596
Analogrithems <Analogrithems@192.168.0.113>
2009-07-21 06:04:05 -1000

    playing with settings format

--- controllers/components/settings_handler.php
+++ controllers/components/settings_handler.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 /**
  * SettingsHandler component is a wrapper a way to read and more importantly
  * write to the config/settings.php file.  Designed to work with plugins also
- * 
+ *
  * @author Analogrithems
  * @version 1.0
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
@@ -10,54 +10,76 @@
  */
 define('PLUGINS', APP_PATH.DS.'plugins');
 class SettingsHandlerComponent extends Object {
-	/**
-	 * Reference to the controller
-	 * 
-	 * @var object
-	 * @access private
-	 */
-	var $__controller = null;
-	var $settingsFile;
-	var $_settingsName = 'settings';
-	var $_settings = array();
-	
-	function initialize(&$controller) {
-		$this->__controller = &$controller;
-		$this->settingsFile = ROOT.DS.APP_DIR.DS.'config'.DS.'settings.php';
-		Configure::load($this->_settingsName);
-		$this->_settings = Configure::read($this->_settingsName);
-	}
-	
-	function getSettings(){
-		return $this->_settings;
-	}
-	
-	function setSettings($type, $data){
-		$date = array_merge($this->_settings[$this->_settingsName], $data);
-		$content = '';
-		foreach ($data as $key => $value) {
-			$content .= "\$config['$type']['$key']";
-			if (is_array($value)) {
-				$content .= " = array(";
-				foreach ($value as $key1 => $value2) {
-					$value2 = addslashes($value2);
-					$content .= "'$key1' => '$value2', ";
-				}
-				$content .= ");\n";
-			} else {
-				$value = addslashes($value);
-				$content .= " = '$value';\n";
-			}
-		}
-		if (!class_exists('File')) {
-			require LIBS . 'file.php';
-		}
+        /**
+         * Reference to the controller
+         *
+         * @var object
+         * @access private
+         */
+        var $__controller = null;
+        var $settingsFile;
+        var $_settingsName = 'settings';
+        var $_settings = array();
+ 
+        function initialize(&$controller) {
+                $this->__controller = &$controller;
+                $this->settingsFile = ROOT.DS.APP_DIR.DS.'config'.DS.'settings.php';
+                Configure::load($this->_settingsName);
+                $this->_settings = Configure::read($this->_settingsName);
+        }
+ 
+        function getSettings(){
+                return $this->_settings;
+        }
+ 
+        function setSettings($data){
+                $data = array_merge($this->_settings[$this->_settingsName], $data);
+                $configs = $this->expandArray($data);
+                $this->log("Settings Data:".print_r($configs,true)."\nfrom:".print_r($data,true),'debug');
+                foreach ($configs as $config) {
+                        $content .= $config;
+                }
+ 
+                if (!class_exists('File')) {
+                        require LIBS . 'file.php';
+                }
+ 
+                $fileClass = new File($this->settingsFile,true,'0744');
+                $content = "<?php\n".$content."\n?>";
+                if ($fileClass->writable()) {
+                        $fileClass->write($content);
+                }
+        }
 
-		$fileClass = new File($this->settingsFile,true,'0744');
-		$content = "<?php\n\$config = array();\n".$content."\n?>";
-		if ($fileClass->writable()) {
-			$fileClass->write($content);
-		}
-	}
+        function expandArray($options, $prefix = null){
+                $results = array();
+                if(isset($prefix) && !empty($prefix)){
+                	$prefix = "$prefix.";
+                }else{
+                	$prefix = '';
+                }
+                foreach($options as $key => $value){
+                	$key = $prefix.$key;
+                        if(is_array($value)){
+                                $tres =  $this->expandArray($value, $key);
+                                foreach($tres as $res){
+                                	$results[] = $res;
+                                }
+                                $this->log("Expanding Array again :".print_r($results,true),'debug');
+                        }else{
+                                $results[] = "Configure::write ('$key', '$value');\n";
+                        }
+                }    
+                         
+                return $results;
+        }
+
+        function AutoSet($attribute, $options){
+                $this->_settings[$this->_settingsName]['auto'][$attribute] = $options;
+        }
+
+        function SyncWith($attribute, $options){
+                $this->_settings[$this->_settingsName]['sync'][$attribute] = $options;
+        }
 }
 ?>
--- plugins/posix/controllers/posix_settings_controller.php
+++ plugins/posix/controllers/posix_settings_controller.php
@@ -1,19 +1,32 @@
 <?php
 class PosixSettingsController extends PosixAppController {
-	var $name = 'PosixSettings';
-	var $uses = array('Posix.PosixSetting');
-	var $components = array('RequestHandler', 'SettingsHandler');
-	var $helpers = array('Form','Html','Javascript', 'Ajax');
-	
-	function index(){
-		if(!empty($this->data)){
-			$this->log("Trying To Save Settings:".print_r($this->data,true),'debug');
-			$this->SettingsHandler->setSettings('settings', $this->data);
-		}
-		$settings = $this->SettingsHandler->getSettings();
-		$this->log("Loading Settings:".print_r($settings,true)."\nCheck Paths:".print_r(Configure::corePaths('cake'),true),'debug');
-	
-		$this->data = $settings;
-	}
+        var $name = 'PosixSettings';
+        var $uses = array('Posix.PosixSetting');
+        var $components = array('RequestHandler', 'SettingsHandler');
+        var $helpers = array('Form','Html','Javascript', 'Ajax');
+ 
+        function index(){
+                if(!empty($this->data)){
+                        $this->log("Trying To Save Settings:".print_r($this->data,true),'debug');
+                        if($this->data['PosixSetting']['autouidnumber'] == true){
+                                $this->SettingsHandler->AutoSet('uidnumber', array('/posix/PosixaccountSchema/findUniqueUidNumber'));
+                                unset($this->data['PosixSetting']['autouidnumber']);
+                        }
+                        if($this->data['PosixSetting']['autogidnumber'] == true){
+                                $this->SettingsHandler->AutoSet('gidnumber', array('/posix/PosixgroupSchema/findUniqueGidNumber'));
+                                unset($this->data['PosixSetting']['autogidnumber']);
+                        }
+                        if($this->data['PosixSetting']['syncwithuniquemember'] == true){
+                                $this->SettingsHandler->SyncWith('uniquemember', array('/posix/PosixgroupSchema/syncGroup'=>array('dn')));
+                                unset($this->data['PosixSetting']['syncwithuniquemember']);
+                        }
+ 
+                        $this->SettingsHandler->setSettings($this->data);
+                }
+                $settings = $this->SettingsHandler->getSettings();
+                $this->log("Loading Settings:".print_r($settings,true)."\nCheck Paths:".print_r(Configure::corePaths('cake'),true),'debug');
+ 
+                $this->data = $settings;
+        }
 }
 ?>
