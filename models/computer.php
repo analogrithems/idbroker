b8c36d02a4babef1177313d71bfa163af7066ce4
analogrithems <analogrithems@gmail.com>
2009-08-04 23:02:03 -1000

    Trying to add tmp dirs

--- models/computer.php
+++ models/computer.php
@@ -7,7 +7,7 @@ class Computer extends AppModel {
 	var $useTable = 'ou=Computers'; // Adapt this parameter to your data
 	var $validate = array(
 		'cn' => array(
-			'rule' => array('custom', "/(?=^.{1,254}$)(^(?:(?!\d+\.|-)[a-zA-Z0-9_\-]{1,63}(?<!-)\.?)+(?:[a-zA-Z]{2,})$)/"),
+			'rule' => array('custom', '/(?=^.{1,254}$)(^(?:(?!\d+\.|-)[a-zA-Z0-9_\-]{1,63}(?<!-)\.?)+(?:[a-zA-Z]{2,})$)/'),
 			'required' => true,
 			'on' => 'create',
 			'message' => 'Must Be A Valid Fully Qualified Domain Name.'
