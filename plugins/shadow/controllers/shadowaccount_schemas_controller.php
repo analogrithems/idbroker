a9ff4cae1f673ce348eb4bbd668d03289c1155f3
analogrithems <analogrithems@gmail.com>
2009-09-15 20:20:39 -0700

    Properly Handel ShadowLastChange and ShadowExpire javascript settings.  This should probably be changed to come from javascript in the future.

--- plugins/shadow/controllers/shadowaccount_schemas_controller.php
+++ plugins/shadow/controllers/shadowaccount_schemas_controller.php
@@ -1,31 +1,38 @@
 <?php
 class ShadowaccountSchemasController extends ShadowAppController {
-	var $name = 'ShadowaccountSchemas';
-	var $uses = array('Shadow.ShadowaccountSchema');
-	var $components = array('RequestHandler', 'Ldap');
-	var $helpers = array('Form','Html','Javascript', 'Ajax');
-	
-	function edit( $id ){
-		if(!empty($this->data)){
-			if(isset($this->data['ShadowaccountSchema']['dn']) && !empty($this->data['ShadowaccountSchema']['dn']) ){
-				$this->ShadowaccountSchema->id = $this->data['ShadowaccountSchema']['dn'];
-			}
+        var $name = 'ShadowaccountSchemas';
+        var $uses = array('Shadow.ShadowaccountSchema');
+        var $components = array('RequestHandler', 'Ldap');
+        var $helpers = array('Form','Html','Javascript', 'Ajax');
 
-			$this->log("What I'm Going to save".print_r($this->data['ShadowaccountSchema'],true)."\nFor the following ID:".$this->ShadowaccountSchema->id,'debug');
-			$result = $this->ShadowaccountSchema->save($this->data);
-			if($result != false){
-				$this->Session->setFlash('Your shadow settings have been updated.');
-			}else{
-				$this->Session->setFlash('Failed to update');
-			}
-		}else{
-			$options['scope'] = 'base';
-			$options['targetDn'] = $id;
-			$options['conditions'] = 'objectclass=Shadowaccount';	
-			$this->data = $this->ShadowaccountSchema->find('first', $options);
-		}
+        function edit( $id ){
+                if(!empty($this->data)){
+                        if(isset($this->data['ShadowaccountSchema']['dn']) && !empty($this->data['ShadowaccountSchema']['dn']) ){
+                                $this->ShadowaccountSchema->id = $this->data['ShadowaccountSchema']['dn'];
+                        }
 
-		$this->layout = 'ajax';
-	}
+                        if(isset($this->data['ShadowaccountSchema']['shadowlastchange']) && !empty($this->data['ShadowaccountSchema']['shadowlastchange']) ){
+                                $this->data['ShadowaccountSchema']['shadowlastchange'] = ceil(strtotime($this->data['ShadowaccountSchema']['shadowlastchange']) / 86400);
+                        }
+                        if(isset($this->data['ShadowaccountSchema']['shadowexpire']) && !empty($this->data['ShadowaccountSchema']['shadowexpire']) ){
+                                $this->data['ShadowaccountSchema']['shadowexpire'] = ceil(strtotime($this->data['ShadowaccountSchema']['shadowexpire']) / 86400);
+                        }
+                        $result = $this->ShadowaccountSchema->save($this->data);
+                        if($result != false){
+                                $this->Session->setFlash('Your shadow settings have been updated.');
+                        }else{
+                                $this->Session->setFlash('Failed to update');
+                        }
+                }else{
+                        $options['scope'] = 'base';
+                        $options['targetDn'] = $id;
+                        $options['conditions'] = 'objectclass=Shadowaccount';
+                        $this->data = $this->ShadowaccountSchema->find('first', $options);
+                }
+
+                $this->layout = 'ajax';
+        }
+
+
 }
 ?>
